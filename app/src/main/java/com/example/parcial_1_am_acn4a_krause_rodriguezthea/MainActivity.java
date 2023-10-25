a152c126b8b89e08c18b8f8d14838593801cfe7c
felikrause <141453056+felikrause@users.noreply.github.com>
2023-10-25 18:51:20 -0300

    commit feat textview us y pass incorrectos

--- app/src/main/java/com/example/parcial_1_am_acn4a_krause_rodriguezthea/MainActivity.java
+++ app/src/main/java/com/example/parcial_1_am_acn4a_krause_rodriguezthea/MainActivity.java
@@ -1,11 +1,11 @@
 package com.example.parcial_1_am_acn4a_krause_rodriguezthea;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView; // Importar la clase TextView
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     private EditText editTextUsername;
     private EditText editTextPassword;
     private Button buttonLogin;
+    private TextView textViewError;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,16 +23,20 @@ public class MainActivity extends AppCompatActivity {
         editTextUsername = findViewById(R.id.editTextUsername);
         editTextPassword = findViewById(R.id.editTextPassword);
         buttonLogin = findViewById(R.id.buttonLogin);
+        textViewError = findViewById(R.id.textViewError);
 
         buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String username = editTextUsername.getText().toString();
                 String password = editTextPassword.getText().toString();
+
                 if (username.equals("admin") && password.equals("admin")) {
+
                     Toast.makeText(MainActivity.this, "Inicio de sesión exitoso", Toast.LENGTH_SHORT).show();
                 } else {
-                    Toast.makeText(MainActivity.this, "Inicio de sesión fallido", Toast.LENGTH_SHORT).show();
+                    textViewError.setVisibility(View.VISIBLE);
+                    textViewError.setText("Usuario o contraseña incorrectos");
                 }
             }
         });
