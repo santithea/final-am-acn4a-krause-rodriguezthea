764ca432bdb9a0750f66d1353e07bc4cced06555
santithea <santiago.rodriguezt@davinci.edu.ar>
2023-10-24 23:19:00 -0300

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "parcial-1-am-acn4a-krause-rodriguezthea"
+include(":app")
