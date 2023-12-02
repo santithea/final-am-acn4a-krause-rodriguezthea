ebff01292a8d37576476f53a57a11c52ba91bbbc
santithea <144290486+santithea@users.noreply.github.com>
2023-12-01 23:56:54 -0300

    commit feat HomeActivity.java DetalleProductoActivity.java

--- app/src/main/java/com/example/parcial_1_am_acn4a_krause_rodriguezthea/DetalleProductoActivity.java
+++ app/src/main/java/com/example/parcial_1_am_acn4a_krause_rodriguezthea/DetalleProductoActivity.java
@@ -1,14 +1,39 @@
 package com.example.parcial_1_am_acn4a_krause_rodriguezthea;
 
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.squareup.picasso.Picasso;
 
 public class DetalleProductoActivity extends AppCompatActivity {
+    public static final String EXTRA_NOMBRE_PRODUCTO = "extra_nombre_producto";
+    public static final String EXTRA_DESCRIPCION_PRODUCTO = "extra_descripcion_producto";
+    public static final String EXTRA_FOTO_PRODUCTO = "extra_foto_producto";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalle_producto);
+
+        // Recupera el nombre del producto del intent
+        String nombreProducto = getIntent().getStringExtra(EXTRA_NOMBRE_PRODUCTO);
+        String descripcionProducto = getIntent().getStringExtra(EXTRA_DESCRIPCION_PRODUCTO);
+        String fotoProducto = getIntent().getStringExtra(EXTRA_FOTO_PRODUCTO);
+
+        ImageView imagenProductoImageView = findViewById(R.id.imagenProductoImageView);
+        Picasso.get().load(fotoProducto).into(imagenProductoImageView);
+
+        // Ejemplo: Mostrar el nombre del producto en un TextView
+        TextView nombreProductoTextView = findViewById(R.id.tituloProductoTextView);
+        nombreProductoTextView.setText(nombreProducto);
+
+        TextView descripcionProductoTextView = findViewById(R.id.descripcionProductoTextView);
+        descripcionProductoTextView.setText(descripcionProducto);
+
+
     }
 }
--- app/src/main/java/com/example/parcial_1_am_acn4a_krause_rodriguezthea/HomeActivity.java
+++ app/src/main/java/com/example/parcial_1_am_acn4a_krause_rodriguezthea/HomeActivity.java
@@ -1,14 +1,63 @@
 package com.example.parcial_1_am_acn4a_krause_rodriguezthea;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class HomeActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HomeActivity extends AppCompatActivity implements OnVerMasClickListener {
+
+    private RecyclerView recyclerViewProductos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        recyclerViewProductos = findViewById(R.id.recyclerViewProductos);
+        recyclerViewProductos.setLayoutManager(new LinearLayoutManager(this));
+        List<Producto> listaProductos = obtenerListaDeProductos();
+
+        // Crear el adaptador antes del código de configuración del click listener
+        ProductosAdapter productosAdapter = new ProductosAdapter(listaProductos, this);
+
+        // Configurar el click listener después de crear el adaptador
+        productosAdapter.setOnVerMasClickListener(new OnVerMasClickListener() {
+            @Override
+            public void onVerMasClick(int position) {
+                Producto producto = listaProductos.get(position);
+                Toast.makeText(HomeActivity.this, "Ver más de " + producto.getNombre(), Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(HomeActivity.this, DetalleProductoActivity.class);
+                intent.putExtra(DetalleProductoActivity.EXTRA_NOMBRE_PRODUCTO, producto.getNombre());
+                intent.putExtra(DetalleProductoActivity.EXTRA_DESCRIPCION_PRODUCTO, producto.getDescripcion());
+                intent.putExtra(DetalleProductoActivity.EXTRA_FOTO_PRODUCTO, producto.getUrlImagen());
+                startActivity(intent);
+            }
+        });
+
+        recyclerViewProductos.setAdapter(productosAdapter);
+    }
+
+    private List<Producto> obtenerListaDeProductos() {
+        List<Producto> listaProductos = new ArrayList<>();
+
+        listaProductos.add(new Producto("Air Jordan 1 Low", "Zapatillas Jordan para Hombre", "https://nikearprod.vtexassets.com/arquivos/ids/679745/553558_174_A_PREM.jpg?v=638224543210770000"));
+        listaProductos.add(new Producto("Air Jordan 1 Mid SE Craft", "Zapatillas Jordan para Hombre", "https://nikearprod.vtexassets.com/arquivos/ids/530374-1200-1200?v=638161363745530000&width=1200&height=1200&aspect=true"));
+        listaProductos.add(new Producto("Air Jordan 1 Zoom CMFT 2", "Zapatillas Jordan para Hombre", "https://nikearprod.vtexassets.com/arquivos/ids/659859-1200-1200?v=638221826927100000&width=1200&height=1200&aspect=true"));
+        listaProductos.add(new Producto("Air Jordan 2 Retro Low", "Zapatillas Jordan para Hombre", "https://nikearprod.vtexassets.com/arquivos/ids/533533-1200-1200?v=638161410875770000&width=1200&height=1200&aspect=true"));
+
+        return listaProductos;
+    }
+
+
+    public void onVerMasClick(int position) {
+
     }
 }
+
